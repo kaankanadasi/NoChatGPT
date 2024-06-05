86709ae69a22cf51fe6c7fbbedbabd04156b8270
kaankanadasi <kaan.kanadasi@hisarschool.k12.tr>
2024-06-06 00:45:42 +0300

    Update CS50_7.py

--- CS50_P/CS50_7.py
+++ CS50_P/CS50_7.py
@@ -26,7 +26,20 @@ import re
 # {m,n} - m-n repetitions
 
 # or use re.search("..*@..*", email)
-if re.search(".+@.+", email):
+# we use '\' before '.edu' since using '.' in this context would mean a different thing
+# yet, since python interperts '\' as new line, we put 'r' before the string to indicate not to create a new line 
+if re.search(r".+@.+\.edu", email):
+    print("Valid")
+else:
+    print("Invalid")
+
+
+# since we don't want the user to type "my email is kaankanadasi@gmail.com", we should indicate that 
+# the mail is in the  should be the start and the end of the string
+
+# ^ - matches the start of the string 
+# $ - matches the end of the string or just before the newline at the end of the string
+if re.search(r"^.+@.+\.edu$", email):
     print("Valid")
 else:
     print("Invalid")
